e82c307be0c6a90db6787a601cf69da071301ec1
jchute <chute.j.jordan@gmail.com>
2026-02-23 15:15:13 -0500

    Fix demo reset signal using DemoResetLog instead of user.last_login

--- demo/models.py
+++ demo/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+# Create new model to track the last time demo user was reset
+class DemoResetLog(models.Model):
+    last_reset_at = models.DateTimeField(null=True, blank=True)
--- demo/signals.py
+++ demo/signals.py
@@ -4,16 +4,22 @@ from datetime import timedelta
 from django.core.management import call_command
 
 def check_demo_reset(sender, user, request, **kwargs):
-    
+
     # Make sure current user is the demo user
     if user.username != 'demo':
         return
-    
-    # Verify it has been over 24 hours since last time a demo user logged in
-    if user.last_login and timezone.now() - user.last_login > timedelta(hours=24):
-        
-        # Trigger the reset demo management command by using call_command
+
+    from demo.models import DemoResetLog
+
+    # Get or create the reset log (uses last_reset_at instead of user.last_login,
+    # because Django updates user.last_login before this signal handler runs)
+    log, _ = DemoResetLog.objects.get_or_create(pk=1)
+
+    # Trigger a reset if the demo has never been reset, or it has been over 24 hours
+    if log.last_reset_at is None or timezone.now() - log.last_reset_at > timedelta(hours=24):
         call_command('reset_demo')
+        log.last_reset_at = timezone.now()
+        log.save()
 
 # Connect the receiver to the signal
 user_logged_in.connect(check_demo_reset)
