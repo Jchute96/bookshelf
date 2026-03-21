b43cd6500c7963ab911bc0f5571a7dadd5325dd6
jchute <chute.j.jordan@gmail.com>
2026-03-21 19:43:55 -0400

    chore: switched email provider for password reset from sendgrid to gmail SMTP

--- config/settings.py
+++ config/settings.py
@@ -68,7 +68,6 @@ INSTALLED_APPS = [
     'demo.apps.DemoConfig',
     'cloudinary_storage',
     'cloudinary',
-    'anymail',
 ]
 
 MIDDLEWARE = [
@@ -180,14 +179,13 @@ LOGOUT_REDIRECT_URL = '/accounts/login/'
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
-# Use SendGrid's Web API instead of SMTP to avoid Railway's port blocking
-EMAIL_BACKEND = 'anymail.backends.sendgrid.EmailBackend'
-
-# SendGrid API key loaded from environment variables
-ANYMAIL = {
-    'SENDGRID_API_KEY': os.environ.get('SENDGRID_API_KEY'),
-}
-# Email address users see as the sender in their inbox
+# Gmail SMTP configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = 'BookShelf <bookshelf.app.noreply@gmail.com>'
 
 
